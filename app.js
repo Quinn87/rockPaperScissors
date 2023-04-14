8968c4b64c7673f36d946856b426c2b9abed5f8c
Brandon Quinn <github@thequinn.org>
2023-04-14 12:28:32 -0500

    reworked the computers choice and added the playRound function.

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 
 //prompt for the players choice
-const playersChoice = prompt("Enter rock, paper, or scissors.").toLowerCase();
+let playerSelection = prompt("Enter rock, paper, or scissors.").toLowerCase();
 //continue to prompt if the players choise is invalid
-while (playersChoice !== "rock" && playersChoice !== "paper" && playersChoice !== "scissors") {
-    const playersChoice = prompt("Invalid choose. Enter rock, paper, or scissors.").toLowerCase();
+while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+    playerSelection = prompt("Invalid choose. Enter rock, paper, or scissors.").toLowerCase();
 }
 
 //generate the computer's choice
-function getComputerChoice (){
+function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-    if (randomNumber <= 33){
-        return "Rock"
+    if (randomNumber <= 33) {
+        return "rock";
     }
-    else if (randomNumber > 66){
-        return "Paper"
+    else if (randomNumber > 66) {
+        return "paper";
     }
     else {
-        return "Scissors"
+        return "scissors";
     }
 }
+
+const computerSelection = computerChoice();
+
+//play round
+function playRound(playerSelection, computerSelection) {
+    //player wins
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "rock")){
+        let roundResult = `You win! ${playerSelection} beats ${computerSelection}!`;
+        return roundResult;
+    }
+
+    else if (playerSelection === computerSelection){
+        let roundResult = `Tie! You and the Computer both chose ${computerSelection}!`;
+        return roundResult;
+    }
+
+    //player loses
+    else {
+        let roundResult = `You lose. ${computerSelection} beats ${playerSelection}!`;
+        return roundResult;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
