2e208ccc39422f35caccff7cf866ef5630702c3c
Brandon Quinn <github@thequinn.org>
2023-05-04 21:40:33 -0500

    Removed game(). Made buttons eventListener trigger

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
+const buttons = document.querySelectorAll('button');
+const playerScoreDisplay = document.querySelector('#playerScoreDisplay');
+const computerScoreDisplay = document.querySelector('#computerScoreDisplay');
+const result = document.querySelector('#result');
+
 let playerScore = 0;
 let computerScore = 0;
-game();
+// game();
 
-function playerChoice() {
-    //prompt for the players choice
-    let playerSelection = prompt("Enter rock, paper, or scissors.").toLowerCase();
-    //continue to prompt if the players choise is invalid
-    while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        playerSelection = prompt("Invalid choose. Enter rock, paper, or scissors.").toLowerCase();
+buttons.forEach(button => button.addEventListener('click', function () {
+    let playerSelection = this.textContent
+    let computerSelection = computerChoice();
+    let roundResult = playRound(playerSelection, computerSelection);
+
+    if (roundResult === "win") {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        result.textContent = `You win! ${playerSelection} beats ${computerSelection}!`
     }
-    return playerSelection;
-}
+
+    else if (playerSelection === computerSelection) {
+        result.textContent = `Tie! You and the Computer both chose ${computerSelection}!`
+    }
+    else if (roundResult === "lose") {
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        result.textContent = `You lose. ${computerSelection} beats ${playerSelection}!`
+    }
+}));
 
 //generate the computer's choice
 function computerChoice() {
@@ -48,6 +64,8 @@ function playRound(playerSelection, computerSelection) {
         return "lose";
     }
 }
+
+
 
 function game() {
     for (i = 0; i < 5; i++) {
